c6952b79e283285d3eb149dc35fc0124c66be880
山南 <zhangpeichaook@163.com>
2016-08-11 12:33:40 +0800

    Init

---
+++ ProcessMonitor/ServiceMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMonitor
+{
+    public partial class ServiceMain : ServiceBase
+    {
+        public ServiceMain()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
